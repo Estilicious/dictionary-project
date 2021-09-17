9a5459aad930aba98407a35a45ed875f526f1809
Estilicious <81353867+Estilicious@users.noreply.github.com>
2021-09-17 18:18:33 +0900

    Updated dictionary app

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ export default function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo img-fluid" alt="logo" />
-        Hello
       </header>
     </div>
   );
