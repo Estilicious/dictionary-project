2f3b9e5841b72bfa880cc0608bb9af2c82333e74
Estilicious <81353867+Estilicious@users.noreply.github.com>
2021-09-18 00:04:20 +0900

    set up dictionary app search engine

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from "./esylogo1.png"
+import Dictionary from "./Dictionary.js";
 import './App.css';
 
 export default function App() {
@@ -7,6 +8,12 @@ export default function App() {
       <header className="App-header">
         <img src={logo} className="App-logo img-fluid" alt="logo" />
       </header>
+      <main>
+        <Dictionary />
+        </main>
+      <footer className="App-footer">
+        <small>Coded by Esther</small>
+      </footer>
     </div>
   );
 }
